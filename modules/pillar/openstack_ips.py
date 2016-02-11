0fa0ee3c19128f37ac34f972241fdb318de9d11f
Daniel Wallace <danielwallace@gtmanfred.com>
2016-02-11 03:25:57 +0000

    use runner client class to quiet output

--- modules/pillar/openstack_ips.py
+++ modules/pillar/openstack_ips.py
@@ -3,6 +3,8 @@ Run salt \* network.interfaces --out yaml | python ipsetup.py
 """
 import logging
 import pprint
+import salt.config
+import salt.runner
 
 log = logging.getLogger(__name__)
 
@@ -11,7 +13,10 @@ def ext_pillar(minion_id, pillar, **kwargs):
     '''
     Runner for setting up ips for openstack
     '''
-    ifs = __salt__.saltutil.runner('mine.get', tgt='*', fun='network.interfaces')
+    opts = salt.config.master_config('/etc/salt/master')
+    opts['quiet'] = True
+    runner = salt.runner.RunnerClient(opts)
+    ifs = runner.cmd('mine.get', kwarg={'tgt': '*', 'fun': 'network.interfaces'})
     log.debug('Interfaces to setup:\n%s', pprint.pformat(ifs))
     ips = {
         'ips': {
